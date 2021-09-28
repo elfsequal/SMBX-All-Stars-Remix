0ca539132dc3fb7bb096a00b68bc0a946b6e1e96
elfsequal <elfsequal2@gmail.com>
2021-09-27 22:34:30 -0400

    Update

---
+++ SMB1 World 3 Classic/luna.lua
@@ -0,0 +1,34 @@
+--------------------------------------------------
+-- Level code
+-- Created 8:36 2021-9-17
+--------------------------------------------------
+
+-- Run code on level start
+function onStart()
+	Player.setCostume(CHARACTER_MARIO, "SMB1 Mario", true)
+	player.powerup = PLAYER_SMALL;
+end
+
+-- Run code every frame (~1/65 second)
+-- (code will be executed before game logic will be processed)
+function onTick()
+	player.reservePowerup = 0
+end
+
+-- Run code when internal event of the SMBX Engine has been triggered
+-- eventName - name of triggered event
+function onEvent(eventName)
+    --Your code here
+	if(eventName == "BowserDeath") then
+		--Audio.MusicChange(player.section, "silence")
+		Audio.playSFX("SMB1 Classic Boss Fanfare.ogg")
+	end
+end
+
+function onInputUpdate()
+if player.altJumpKeyPressing then
+player.altJumpKeyPressing = false
+player.jumpKeyPressing = true
+end
+end
+
